332db657dc287a402696992ae2d0d4261fe7bede
XXXJumpingFrogXXX <wangqx27@outlook.com>
2024-10-16 15:46:50 +0800

    Modifications on FastAPI

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from fastapi.middleware.cors import CORSMiddleware
 from chat.router import router as chat_router
 # from piggy.router import router as piggy_router
 
+# Create a FastAPI application instance with custom documentation URL
 app = FastAPI(
     docs_url="/sugar-ai/docs",
 )
 
+# Include the chat router with a specified prefix for endpoint paths
 app.include_router(chat_router, prefix="/sugar-ai/chat")
+# Include the piggy router with a specified prefix for endpoint paths (currently commented out)
 # app.include_router(piggy_router, prefix="/sugar-ai/piggy")
 
+# Add CORS middleware to allow cross-origin requests from any origin
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"],  # Allow requests from any origin
+    allow_credentials=True,  # Allow sending of credentials (e.g., cookies)
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
 )
