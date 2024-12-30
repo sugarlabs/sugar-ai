c632bfbee69c9503e41c8c19dee91b1c4ebdb42f
XXXJumpingFrogXXX <wangqx27@outlook.com>
2024-12-31 06:57:52 +0800

    Incorporate Piggy Activity Code

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from chat.router import router as chat_router
-# from piggy.router import router as piggy_router
+from Piggy.router import router as piggy_router
 
 app = FastAPI(
     docs_url="/sugar-ai/docs",
 )
 
 app.include_router(chat_router, prefix="/sugar-ai/chat")
-# app.include_router(piggy_router, prefix="/sugar-ai/piggy")
+app.include_router(piggy_router, prefix="/sugar-ai/piggy")
 
 app.add_middleware(
     CORSMiddleware,
